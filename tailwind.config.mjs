708f72f8bc7f8fdd5724a38ce95c940a4323a4dd
ERIKSON ROJAS <113566221+Eriksoncucuta@users.noreply.github.com>
2025-05-30 18:51:57 -0500

    Restructure website with new sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 const { nextui } = require("@nextui-org/react");
+
 export default {
   content: [
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui(),
+    require('@tailwindcss/animate')
+  ],
 };
